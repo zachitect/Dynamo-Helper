a39debd08c596d4c0cb1ec5b8ab9962b9f3a2a4d
Zach X.G. Zheng <zach@zachitect.com>
2024-02-14 10:16:32 +1100

    Update 02_external_file.py

--- 02_external_file.py
+++ 02_external_file.py
@@ -10,13 +10,13 @@ def txt_reader(file_path):
     return content
 
 #list all files of specific extension from directory
-def files_from_directory(dir_path, extension):
+def files_from_directory(dir_path, dot_ext):
     file_paths = []
     for path in listdir(dir_path):
         full_path = join(dir_path, path)
         if isfile(full_path): #confirm that the path is a file not a directory
 	    file_split = os.path.splitext(path)
 	    if len(file_split) == 2: #root path + extension name
-	        if file_split[1].lower() == extension.lower(): #check extension name
-		    files_paths.append(full_path)
+	        if file_split[1].lower() == dot_ext.lower(): #check extension name
+		        file_paths.append(full_path)
     return file_paths
